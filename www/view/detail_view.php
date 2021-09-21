ea2d90bf0bb9cdda7f7b2620361ff1ce40e97aa8
tyakku11 <tyakku.th@gmail.com>
2021-09-21 22:34:10 +0900

    ヘッダー、クラス名追加#12

--- www/view/detail_view.php
+++ www/view/detail_view.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="ja">
   <head>
+    <?php include VIEW_PATH . 'templates/head.php'; ?>  
     <meta charset="UTF-8">
     <title>購入明細</title>
+    <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'detail.css'); ?>">
   </head>
 
   <body>
+    <?php include VIEW_PATH . 'templates/header_logined.php'; ?>  
     <h1>購入明細</h1>
+    <div class="container">
 
     <?php include VIEW_PATH. 'templates/messages.php'; ?>
 
     <!-- 購入明細 -->
-    <table>
-      <thead>
+    <table class="table table-bordered">
+        <thead class="thead-light">
         <tr>
           <th>注文番号</th>
           <th>購入日時</th>
@@ -37,8 +41,8 @@
     </table>
 
     <!-- 購入明細 -->
-    <table>
-      <thead>
+    <table class="table table-bordered">
+        <thead class="thead-light">
         <tr>
           <th>商品名</th>
           <th>価格</th>
@@ -57,6 +61,7 @@
       <?php } ?>
       </tbody>
     </table>
+    </div>
   </body>
 </html>
 
--- www/view/history_view.php
+++ www/view/history_view.php
@@ -2,19 +2,23 @@
 <html lang="ja">
   <head>
     <meta charset="UTF-8">
+    <?php include VIEW_PATH . 'templates/head.php'; ?>
     <title>購入履歴</title>
+    <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'history.css'); ?>">
   </head>
 
   <body>
+    <?php include VIEW_PATH . 'templates/header_logined.php'; ?>  
     <h1>購入履歴</h1>
+    <div class="container">
 
     
     <?php include VIEW_PATH. 'templates/messages.php'; ?>
 
     
     <?php if(!empty($histories)){ ?>
-    <table>
-      <thead>
+    <table class="table table-bordered">
+        <thead class="thead-light">
         <tr>
           <th>注文番号</th>
           <th>購入日時</th>
@@ -41,5 +45,6 @@
     <?php }else{ ?>
     <p>購入履歴がありません。</p>
     <?php } ?>
+    </div>
   </body>
 </html>
